46689f22e3420b83f9211e8b9c891d60dc02bd3d
sally03915 <sally03915@gmail.com>
2026-01-27 14:11:33 +0900

    test-260127-1415pm

--- front/next.config.js
+++ front/next.config.js
@@ -4,15 +4,15 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8484/api/:path*', // 백엔드 주소
+        destination: 'http://localhost:8080/api/:path*', // 백엔드 주소
       },
       {
         source: '/auth/:path*',
-        destination: 'http://localhost:8484/auth/:path*', // 백엔드 주소
+        destination: 'http://localhost:8080/auth/:path*', // 백엔드 주소
       },
       {
         source: '/oauth/:path*',
-        destination: 'http://localhost:8484/oauth/:path*', // 백엔드 주소
+        destination: 'http://localhost:8080/oauth/:path*', // 백엔드 주소
       },
     ]
   },
