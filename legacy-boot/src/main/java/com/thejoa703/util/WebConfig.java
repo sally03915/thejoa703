132a6f0687020d32c1d1e465ea2ed428846f6a8a
sally03915 <sally03915@gmail.com>
2026-01-29 22:56:13 +0900

    test-260129-2255pm

--- legacy-boot/src/main/java/com/thejoa703/util/WebConfig.java
+++ legacy-boot/src/main/java/com/thejoa703/util/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
-	@Value("${upload.path}") private String uploadPath;     //    /upload/ 경로요청
+	@Value("${upload.path}") private String uploadPath;     //    /upload/ 경로요청    
 	@Value("${resource.path}") private String resourcePath; //    C:/upload
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) { 
